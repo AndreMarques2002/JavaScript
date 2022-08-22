02c9674fbde36154a34926ae999c6f2cda25a13a
AndreMarques2002 <andre-msilva2002@hotmail.com>
2022-08-21 22:58:45 -0300

    commit inicial

---
+++ Calculadora/calculator.js
@@ -0,0 +1,5 @@
+const numberButtons = documentSeletorAll('[number]')
+const deleteButton = documentSeletorAll('[delete]')
+const equalsButton = documentSeletorAll('[equals]')
+const operatorButtons = documentSeletorAll('[operator]')
+
